d38e3c048531551bd349dc207e9170c8aac52732
Mopecat <liuyangdiv@163.com>
2019-10-22 14:05:06 +0800

    单向链表结构图&js示例 :tada: :tada:

--- Javascript强化/数据结构与算法篇/数据结构.js
+++ Javascript强化/数据结构与算法篇/数据结构.js
@@ -45,4 +45,63 @@ console.log(stack.stack);
 
 // 链表 常见的链表：单向链表、双向链表、循环链表
 // 操作数据不需要破坏数据的原有结构
-// 单项链表
+// 单向链表
+// 对比图上 每一个node节点上有element元素和next指针
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+class LinkList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  insert(position, element) {
+    let node = new Node(element);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let index = 0; // 从0项开始查找
+      let current = this.head; // 先把链表的头拿出来
+      let previous = null;
+      while (index++ < position) {
+        previous = current;
+        // 如果插入的位置还没有元素将current赋值为null后面用来抛出错误
+        current ? (current = current.next) : (current = null);
+      }
+      if (!current) {
+        throw new Error(`当前链表没有第${position}项`);
+      }
+      // 当插入到第0个时也就是第一个，previous为null，因为前面没有节点了 所以需要处理一下
+      previous ? (previous.next = node) : (this.head = node);
+
+      node.next = current;
+    }
+    this.length++;
+  }
+
+  append(element) {
+    let node = new Node(element);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let index = 0; // 从0项开始查找
+      let current = this.head; // 先把链表的头拿出来
+      // 从头开始查找，直到找到最后一项，让最后一项的next指向当前append的node
+      while (++index < this.length) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  }
+}
+let ll = new LinkList();
+ll.append(1);
+ll.append(2);
+ll.append(3);
+ll.insert(0, 100);
+ll.insert(5, 400);
+console.log(JSON.stringify(ll));
