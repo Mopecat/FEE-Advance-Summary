96998a608d96ba8cb2e8dac7ed38af773a75bb67
Mopecat <liuyangdiv@163.com>
2020-01-05 17:03:04 +0800

    gzip && cache

--- Node/http/http-server/dist/server.js
+++ Node/http/http-server/dist/server.js
@@ -15,6 +15,10 @@ var _util = _interopRequireDefault(require("util"));
 
 var _url = _interopRequireDefault(require("url"));
 
+var _zlib = _interopRequireDefault(require("zlib"));
+
+var _crypto = _interopRequireDefault(require("crypto"));
+
 var _mime = _interopRequireDefault(require("mime"));
 
 var _chalk = _interopRequireDefault(require("chalk"));
@@ -55,6 +59,48 @@ class Server {
     } catch (e) {
       this.sendError(e, res);
     }
+  }
+
+  cache(absPath, statObj, req, res) {
+    res.setHeader("Cache-Control", "max-age=10");
+    let lastModified = statObj.ctime.toGMTString();
+
+    let Etag = _crypto.default.createHash("md5").update(_fs.default.readFileSync(absPath)).digest("base64");
+
+    res.setHeader("Last-Modified", lastModified);
+    res.setHeader("Etag", Etag);
+    let ifModifiedSince = req.headers["if-modified-since"];
+    let ifNoneMatch = req.headers["if-none-match"];
+
+    if (ifModifiedSince || ifNoneMatch) {
+      if (ifModifiedSince !== lastModified || ifNoneMatch !== Etag) {
+        return false;
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  gzip(absPath, statObj, req, res) {
+    let encoding = req.headers["accept-encoding"];
+
+    if (encoding) {
+      if (encoding.match(/gzip/)) {
+        // 设置响应头部为gzip压缩格式
+        res.setHeader("Content-Encoding", "gzip"); // 返回gzip转化流
+
+        return _zlib.default.createGzip();
+      } else if (encoding.match(/deflate/)) {
+        // 设置响应头部为deflate压缩格式
+        res.setHeader("Content-Encoding", "deflate"); // 返回deflate转化流
+
+        return _zlib.default.createDeflate();
+      }
+    }
+
+    return false;
   } // 发送文件
 
 
@@ -70,9 +116,17 @@ class Server {
       res.setHeader("Content-Type", "text/html;charset=utf-8");
       res.end(templateStr);
     } else {
+      let cache = this.cache(absPath, statObj, req, res); // 再文件发送过程中，如果浏览器支持压缩，我需要对浏览器的内容先压缩再返回
+
+      let flag = this.gzip(absPath, statObj, req, res);
       res.setHeader("Content-Type", _mime.default.getType(absPath) + ";charset=utf-8");
 
-      _fs.default.createReadStream(absPath).pipe(res);
+      if (!flag) {
+        _fs.default.createReadStream(absPath).pipe(res);
+      } else {
+        _fs.default.createReadStream(absPath).pipe(flag) // 调用转化流 将返回文件压缩后 再返回给客户端
+        .pipe(res);
+      }
     }
   } // 返回错误
 
--- Node/http/http-server/src/server.js
+++ Node/http/http-server/src/server.js
@@ -3,6 +3,8 @@ import fs from "fs";
 import path from "path";
 import util from "util";
 import url from "url";
+import zlib from "zlib";
+import crypto from "crypto";
 let { stat, access, readdir } = fs.promises;
 let template = fs.readFileSync(
   path.resolve(__dirname, "../template.html"),
@@ -31,6 +33,43 @@ class Server {
       this.sendError(e, res);
     }
   }
+  cache(absPath, statObj, req, res) {
+    res.setHeader("Cache-Control", "max-age=10");
+    let lastModified = statObj.ctime.toGMTString();
+    let Etag = crypto
+      .createHash("md5")
+      .update(fs.readFileSync(absPath))
+      .digest("base64");
+    res.setHeader("Last-Modified", lastModified);
+    res.setHeader("Etag", Etag);
+    let ifModifiedSince = req.headers["if-modified-since"];
+    let ifNoneMatch = req.headers["if-none-match"];
+    if (ifModifiedSince || ifNoneMatch) {
+      if (ifModifiedSince !== lastModified || ifNoneMatch !== Etag) {
+        return false;
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+  gzip(absPath, statObj, req, res) {
+    let encoding = req.headers["accept-encoding"];
+    if (encoding) {
+      if (encoding.match(/gzip/)) {
+        // 设置响应头部为gzip压缩格式
+        res.setHeader("Content-Encoding", "gzip");
+        // 返回gzip转化流
+        return zlib.createGzip();
+      } else if (encoding.match(/deflate/)) {
+        // 设置响应头部为deflate压缩格式
+        res.setHeader("Content-Encoding", "deflate");
+        // 返回deflate转化流
+        return zlib.createDeflate();
+      }
+    }
+    return false;
+  }
   // 发送文件
   async sendFile(absPath, statObj, pathname, req, res) {
     if (statObj.isDirectory()) {
@@ -43,8 +82,17 @@ class Server {
       res.setHeader("Content-Type", "text/html;charset=utf-8");
       res.end(templateStr);
     } else {
+      let cache = this.cache(absPath, statObj, req, res);
+      // 再文件发送过程中，如果浏览器支持压缩，我需要对浏览器的内容先压缩再返回
+      let flag = this.gzip(absPath, statObj, req, res);
       res.setHeader("Content-Type", mime.getType(absPath) + ";charset=utf-8");
-      fs.createReadStream(absPath).pipe(res);
+      if (!flag) {
+        fs.createReadStream(absPath).pipe(res);
+      } else {
+        fs.createReadStream(absPath)
+          .pipe(flag) // 调用转化流 将返回文件压缩后 再返回给客户端
+          .pipe(res);
+      }
     }
   }
   // 返回错误
